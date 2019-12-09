0846950bde93a06511ab093018c2b5f38fe9fd97
zeromsi <www.zeromsi2@gmail.com>
2019-12-09 16:51:04 +0600

    first commit

---
+++ src/main/java/com/example/tektoncdspringbootgradle/TektoncdSpringBootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tektoncdspringbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TektoncdSpringBootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TektoncdSpringBootGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tektoncdspringbootgradle/TektoncdSpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tektoncdspringbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TektoncdSpringBootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
