61bddd66f0cd0e7300e827a8ee7e38163c3da40a
zeromsi <www.zeromsi2@gmail.com>
2020-02-11 14:27:30 +0600

    add script that replaces nexus repo credentials

--- script_to_set_args_buildfile.sh
+++ script_to_set_args_buildfile.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
-sed -i 's=buildscript { =
-buildscript {
-	repositories { 
-		maven { 
-			credentials { 
-				username '"$NEXUS_USER"' 
-				password '"$NEXUS_PASSWORD"' 
-				} 
-			url '"$NEXUS_PUBLIC_URL"' 
-		} 
-	} =g' build.g radle
+sed -i 's=dependencies=buildscript { repositories { maven { credentials { username '"$NEXUS_USER"' password '"$NEXUS_PASSWORD"' } url '"$NEXUS_PUBLIC_URL"' } } dependencies=g' build.g radle
 
-sed -i 's=dependencies {
-	implementation=
-repositories { 
-	maven { 
-		credentials { 
-			username '"$NEXUS_USER"' 
-			password '"$NEXUS_PASSWORD"' 
-			} 
-		url '"$NEXUS_PUBLIC_URL"'
-		} 
-	} 
-dependencies {
-	implementation=g' build.gradle
 
 cat build.gradle
 
